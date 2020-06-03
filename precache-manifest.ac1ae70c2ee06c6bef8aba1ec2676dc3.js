b3b82591e99c8ebc2f62c57f2ede9a4ae8dd2426
Aneta Jóźwiak <aneta_jozwiak@op.pl>
2020-06-03 12:08:14 +0200

    Updates

---
+++ precache-manifest.ac1ae70c2ee06c6bef8aba1ec2676dc3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e657318c1ff48ab6a7c7408846677dc",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "53ef7e55ad5cff7a3a77",
+    "url": "/weather-app/static/css/main.3c2caca9.chunk.css"
+  },
+  {
+    "revision": "7e8b82ed1129ff4ff362",
+    "url": "/weather-app/static/js/2.791aeb16.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/weather-app/static/js/2.791aeb16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53ef7e55ad5cff7a3a77",
+    "url": "/weather-app/static/js/main.ba9a3b02.chunk.js"
+  },
+  {
+    "revision": "12ef64cf8e1aad5a2dd2",
+    "url": "/weather-app/static/js/runtime-main.c90f5b59.js"
+  },
+  {
+    "revision": "98030622ef64249343878af98248620d",
+    "url": "/weather-app/static/media/cl.98030622.jpg"
+  },
+  {
+    "revision": "154ab04663a35eba9af6d2d67e3caf37",
+    "url": "/weather-app/static/media/cloud.154ab046.jpg"
+  },
+  {
+    "revision": "6e6b261cd064b4ce4f0efe2aba42e287",
+    "url": "/weather-app/static/media/mist.6e6b261c.jpg"
+  },
+  {
+    "revision": "32f461b58c5a62bee0b01a9aa341ad8a",
+    "url": "/weather-app/static/media/rain.32f461b5.jpg"
+  },
+  {
+    "revision": "813907dfdca3c62629a23563f137ec7b",
+    "url": "/weather-app/static/media/snow.813907df.jpg"
+  },
+  {
+    "revision": "509c68483664e04c6901802798c02340",
+    "url": "/weather-app/static/media/sun.509c6848.jpg"
+  }
+]);
